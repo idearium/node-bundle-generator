4af14429612910fa5d3bd947f6b40d0d9705e74d
Allan Chau <allan.chau@icloud.com>
2021-06-01 10:23:26 +0930

    add css

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,23 @@
-'use strict';
+"use strict";
 
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    entry: {
-        bundle: path.join(__dirname, 'index.js'),
-    },
-    mode: 'production',
-    output: {
-        filename: '[name].[hash].js',
-        path: path.join(__dirname, 'dist'),
-    },
+  entry: {
+    main: path.join(__dirname, "index.js"),
+    styles: path.join(__dirname, "styles.scss"),
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
+      },
+    ],
+  },
+  output: {
+    filename: "[name].[hash].js",
+    path: path.join(__dirname, "dist"),
+  },
 };
